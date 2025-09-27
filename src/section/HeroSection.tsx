00ac6991f6b36340fccf7d0790eb50a6b7dc9607
Mr-Naqash-Developer <mr.naqash.developer@gmail.com>
2025-09-27 19:57:13 +0500

    Error

--- src/section/HeroSection.tsx
+++ src/section/HeroSection.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import Image from "next/image";
 import Button from "@/components/Button";
+import bg from "../assets/bg.avif";
 
 const HeroSection = () => {
   return (
@@ -20,43 +21,56 @@ const HeroSection = () => {
           <hr className="hidden sm:block w-[80px] border-[#575757]" />
         </div>
 
-        {/* Hero Title */}
-        <div className="flex flex-col gap-6 sm:gap-10">
+        {/* Hero Title with BG behind */}
+        <div className="relative flex flex-col gap-6 sm:gap-10">
+          {/* Background image BEHIND text */}
+          <div className="absolute sm:-right-[100px] top-[90%] sm:top-[70%] -translate-y-1/2 -z-10 opacity-70 pointer-events-none">
+            <Image
+              src={bg}
+              alt="background"
+              width={600}
+              height={600}
+              priority
+              unoptimized
+              className="object-contain w-[300] h-[300] sm:w-[600] sm:h-[600] "
+            />
+          </div>
+
+          {/* Titles */}
           <div>
             {/* First row */}
             <div className="flex flex-col sm:flex-row sm:items-end sm:gap-6 mb-6">
               <span className="text-4xl sm:text-6xl md:text-7xl lg:text-[75px] border-b font-bold text-[#C9F31D] leading-none mb-2 sm:mb-0 w-fit">
                 Senior
               </span>
-              <h1 className="text-5xl sm:text-7xl md:text-8xl lg:text-[120px] xl:text-[150px] font-bold uppercase leading-none">
+              <h1 className="text-5xl sm:text-7xl md:text-8xl lg:text-[150px] font-bold uppercase leading-none">
                 App
               </h1>
               <Image
-                width={120}
-                height={120}
-                className="w-12 h-12 sm:w-16 sm:h-16 md:w-20 md:h-20 lg:w-[100px] lg:h-[100px] xl:w-[120px] xl:h-[120px] mt-4 sm:mt-0"
+                width={100}
+                height={100}
+                className="w-12 h-12 sm:w-16 sm:h-16 md:w-24 md:h-24 lg:w-[120px] lg:h-[120px] mt-4 sm:mt-0"
                 src="/star.svg"
                 alt="star"
               />
             </div>
 
-            {/* Second row */}
             <div className="flex flex-col sm:flex-row sm:items-center sm:gap-4">
               <Image
-                width={350}
-                height={80}
-                className="w-32 h-8 sm:w-40 sm:h-10 md:w-52 md:h-12 lg:w-[280px] lg:h-[65px] xl:w-[350px] xl:h-[80px] mt-4 sm:mt-0"
+                width={100}
+                height={100}
+                className="w-28 h-8 sm:w-40 sm:h-10 md:w-60 md:h-14 lg:w-[350px] lg:h-[80px] mt-4 sm:mt-0"
                 src="/rings.svg"
                 alt="rings"
               />
-              <h1 className="text-5xl sm:text-7xl md:text-8xl lg:text-[120px] xl:text-[150px] font-bold uppercase leading-none">
+              <h1 className="text-5xl sm:text-7xl md:text-8xl lg:text-[150px] font-bold uppercase leading-none">
                 Developer
               </h1>
             </div>
           </div>
 
           {/* Paragraph */}
-          <p className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl max-w-lg md:max-w-2xl text-[#D2D2D2] mt-6 sm:mt-2 text-center sm:text-left">
+          <p className="text-base sm:text-lg md:text-xl lg:text-2xl max-w-lg md:max-w-2xl text-[#D2D2D2] mt-6 sm:mt-0 text-center sm:text-left">
             I believe in the power of innovation and creativity to transform
             your ideas into exceptional digital experiences. Working closely
             with you, I strive to bring your vision to life.
@@ -64,7 +78,7 @@ const HeroSection = () => {
         </div>
 
         {/* CTA Button */}
-        <div className="mt-8 sm:mt-12 md:mt-16 lg:mt-20 flex justify-center sm:justify-start">
+        <div className="mt-10 sm:mt-16 md:mt-20 flex justify-center sm:justify-start">
           <Button text="LET'S TALK" />
         </div>
       </div>
